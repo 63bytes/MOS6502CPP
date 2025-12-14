a8b8b02bb5cb176edc6416c545d3fb00877e3584
jiblert <gilbert.webebr1@gmail.com>
2025-12-14 17:30:06 +0000

    update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <cstdint>
 #include <iostream>
 
-int constexpr MEMORY_MAX = 0xffff;
+uint16_t constexpr MEMORY_MAX = 0xffff;
 int constexpr MICRO_CYCLES = 8;
 uint8_t constexpr SIGN_BIT = 0b10000000;
+uint8_t constexpr STACK_START = 0xFF;
+uint16_t constexpr STACK_PAGE = 0x0100;
+uint8_t constexpr HIGH = 0xff00;
+uint8_t constexpr LOW = 0x00ff;
+uint16_t constexpr BREAK_ADDR = 0xFFFE;
+
+uint8_t signedsub8(uint8_t a, uint8_t b) {
+	return a + (~b + 1);
+}
 
 class ProgramFlag {
 private:
@@ -53,20 +62,36 @@ public:
         uint8_t RAM[MEMORY_MAX] = {};
     }
     void test() {
-        (this->*OPCODEDATA[0x69].func)(21);
+        //(this->*OPCODEDATA[0x69].func)(21);
     }
     void Instruction() {
         preExecute();
         execute();
     }
 private:
+	struct INSTRUCTION_OPHAND{
+		uint8_t value;
+		uint16_t address;
+		bool hasValue=true;
+		bool hasAddress=true;
+	};
+	using OPCODE_FUNCTION = void(CPU::*)(INSTRUCTION_OPHAND);
+	struct Opcode {
+		OPCODE_FUNCTION func;
+		uint8_t opcode;
+		AddressingModes addressing_mode;
+		int cycles;
+		int bytes;
+		Opcode(OPCODE_FUNCTION a, uint8_t b, AddressingModes c, int d, int e){func = a;opcode = b;addressing_mode=c;cycles=d;bytes=e;}
+		Opcode(){}
+	};
     uint8_t RAM[MEMORY_MAX] = {};
 
     uint8_t A = 0;
     uint8_t X = 0;
     uint8_t Y = 0;
     uint16_t PC = 0;
-    uint8_t S = 0;
+    uint8_t S = STACK_START;
     uint8_t P = 0;
 
     uint8_t NI = 0b10000000;
@@ -85,20 +110,13 @@ private:
     ProgramFlag Z = ProgramFlag(S, ZI);
     ProgramFlag C = ProgramFlag(S, CI);
 
+
+
     uint8_t OP = 0;
     uint16_t operand = 0;
 
     int Cycle = 0;
-    using OPCODE_FUNCTION = void(CPU::*)(uint16_t);
-    struct Opcode {
-        OPCODE_FUNCTION func;
-        uint8_t opcode;
-        AddressingModes addressing_mode;
-        int cycles;
-        int bytes;
-        Opcode(OPCODE_FUNCTION a, uint8_t b, AddressingModes c, int d, int e){func = a;opcode = b;addressing_mode=c;cycles=d;bytes=e;}
-        Opcode(){}
-    };
+
 	Opcode currentOP;
     Opcode OPCODEDATA[0xff] = {};
     void InitOps() {//!!! add json reader https://stackoverflow.com/questions/32205981/reading-json-files-in-c
@@ -112,101 +130,108 @@ private:
 		OPCODEDATA[0x79] = Opcode(&CPU::OP_ADC, 0x79, ABY, 4, 3);
     }
 
-    uint8_t fetch(const bool Increment = true) {
-        if (Increment) {
-            PC += 1;
-            return RAM[PC - 1];
-        }
-        else {
-            return RAM[PC];
-        }
+    uint8_t fetch(const int Increment = 1) {
+    	PC += Increment;
+        return RAM[PC - Increment];
     }
 
     uint8_t read(uint16_t address) {
         return RAM[address];
     }
 
+	void stack_push8(uint8_t value) {
+	    RAM[STACK_PAGE | S] = value;S--;
+    }
+	void stack_push16(uint16_t value) {
+	    stack_push8(HIGH & value);
+    	stack_push8(LOW & value);
+    }
+
     uint8_t IAL;//indirect address low
     uint8_t ADL;//Adress low
     uint8_t ADH;//Adress high
-    uint8_t DATA;
     uint8_t BAL;//base addresss
     uint8_t BAH;
     uint8_t OFFSET;
 	uint16_t EFA;//effective address
+	INSTRUCTION_OPHAND DATA;
     void preExecute() {
         OP = fetch();
 		currentOP = OPCODEDATA[OP];
+    	DATA.value = 0;
+    	DATA.hasValue = true;
+    	DATA.address = 0;
+    	DATA.hasAddress = true;
         switch (currentOP.addressing_mode) {
             case IMM:
-                DATA = fetch();
+                DATA.value = fetch();
+        		DATA.hasAddress = false;
                 break;
             case ABS:
                 ADL = fetch();
                 ADH = fetch();
-                DATA = (ADH<<8) | ADL;
+                DATA.address = (ADH<<8) | ADL;
                 break;
             case ZP:
                 ADL = fetch();
-                DATA = ADL;
+                DATA.address = ADL;
                 break;
             case ACC://No logic needed
-                DATA = 0;
+        		DATA.hasAddress = false;
                 break;
             case IMP:
-                DATA = 0;
+        		DATA.hasAddress = false;
                 break;
             case INDX://List of Pointers (ZP)
                 IAL = fetch();
                 IAL += X;//Index zero page with X
                 ADL = read(IAL);
                 ADH = read(IAL+1);
-                DATA = (ADH<<8) | ADL;
+                DATA.address = (ADH<<8) | ADL;
             case INDY://Pointer to a list (ZP)
                 IAL = fetch();
                 BAL = read(IAL);
                 BAH = read(IAL+1);
-                DATA = (ADH<<8) | ADL;
+                DATA.address = (ADH<<8) | ADL;
             case ZPX://Zero page list
                 BAL = fetch();
                 BAL += X;
-                DATA = BAL;
+                DATA.address = BAL;
             case ABX:
                 BAL = fetch() + X;
                 BAH = fetch();
-                DATA = (BAH<<8) | BAL;
+                DATA.address = (BAH<<8) | BAL;
             case ABY:
                 BAL = fetch() + Y;
                 BAH = fetch();
-                DATA = (BAH<<8) | BAL;
+                DATA.address = (BAH<<8) | BAL;
             case REL:
                 OFFSET = fetch();
-                DATA = OFFSET;
+                DATA.value = OFFSET;
+        		DATA.address = false;
             case IND:
                 IAL = fetch();
                 ADL = read(IAL);
                 ADH = read(IAL+1);
-                DATA = (ADH<<8) | ADL;
+                DATA.address = (ADH<<8) | ADL;
             case ZPY:
                 BAL = fetch();
                 BAL += X;
-                DATA = BAL;
+                DATA.address = BAL;
         }
+    	if (DATA.hasAddress) {
+    		DATA.value = RAM[DATA.address];
+    	}
     }
 
     void execute() {
         (this->*OPCODEDATA[OP].func)(DATA);
     }
     uint8_t result;
-    void OP_ADC(const uint16_t o) {
-		if (currentOP.addressing_mode==(IMM)){
-			const uint8_t n = o;
-		}
-		else {
-			const uint8_t n = read(o);//read from effective address
-		};
+    void OP_ADC(const INSTRUCTION_OPHAND d) {
+		const uint8_t n = d.value;
         const int s = A & SIGN_BIT;
-        result = A + o;
+        result = A + n;
         if ((P & D) == D) {
             if ((result & 0x0F) > 0x09) {
                 result += 0x06;
@@ -228,18 +253,13 @@ private:
         Z = A == 0;
         std::cout << "[EXECUTE] ADC" << std::endl;
     }
-	void OP_AND(const uint8_t o) {
-		if (currentOP.addressing_mode==(IMM)){
-			const uint8_t n = o;
-		}
-		else {
-			const uint8_t n = read(o);//read from effective address
-		};
-		A &= o;
+	void OP_AND(const INSTRUCTION_OPHAND d) {
+		const uint8_t n = d.value;
+		A &= n;
 		Z = A==0;
 		N = (A&SIGN_BIT) == SIGN_BIT;
 	}
-	void OP_ASL(const uint8_t o) {
+	void OP_ASL(const INSTRUCTION_OPHAND d) {
 		if (OPCODEDATA[OP].addressing_mode==ACC){
 			C = (N&SIGN_BIT)==SIGN_BIT;
 			A << 1;
@@ -247,32 +267,116 @@ private:
 			Z = N==0;
 		}
 		else {
-			C = (RAM[o]&SIGN_BIT)==SIGN_BIT;
-			RAM[o] << 1;
-			N = (RAM[o]&SIGN_BIT)==SIGN_BIT;
-			Z = RAM[o]==0;
+			C = (RAM[d.address]&SIGN_BIT)==SIGN_BIT;
+			RAM[d.address] << 1;
+			N = (RAM[d.address]&SIGN_BIT)==SIGN_BIT;
+			Z = RAM[d.address]==0;
 		};
 	};
-	void OP_BCC(const uint8_t o) {
+	void OP_BCC(const INSTRUCTION_OPHAND d) {
 		if (C==false) {
-			PC += o;//brach if carry reset
+			PC += d.value;//brach if carry reset
 		}
 	};
-	void OP_BCS(const uint8_t o) {
+	void OP_BCS(const INSTRUCTION_OPHAND d) {
 		if (C) {
-			PC += o;//brach if carry reset
+			PC += d.value;//brach if carry reset
 		}
 	};
-	void OP_BEQ(const uint8_t o) {
+	void OP_BEQ(const INSTRUCTION_OPHAND d) {
 		if (Z) {
-			PC += o;
+			PC += d.value;
 		}
 	}
-	void OP_BIT(const uint8_t o) {
-		const uint8_t n =  RAM[o];
+	void OP_BIT(const INSTRUCTION_OPHAND d) {
+		const uint8_t n =  RAM[d.value];
 		N = (n&SIGN_BIT)==SIGN_BIT;
 		V = (n&0b01000000)==0b01000000;
 		result = A & n;
 		Z = result==0;
 	}
+    void OP_BMI(const INSTRUCTION_OPHAND d) {
+	    if (N) {
+	        PC += d.value;
+	    }
+	}
+    void OP_BNE(const INSTRUCTION_OPHAND d) {
+	    if (Z==false) {
+	        PC += d.value;
+	    }
+	}
+    void OP_BPL(const INSTRUCTION_OPHAND d) {
+	    if (N==false) {
+	        PC += d.value;
+	    }
+	}
+    void OP_BRK(const INSTRUCTION_OPHAND d) {
+		PC += 1;
+		stack_push16(PC);
+		stack_push8(P);
+		ADH = RAM[BREAK_ADDR];
+		ADL = RAM[BREAK_ADDR+1];
+		PC = (ADH<<8) | ADL;
+		B = true;
+	}
+    void OP_BVC(const INSTRUCTION_OPHAND d) {
+		if (V==false) {
+			PC += d.value;
+		}
+	}
+    void OP_BVS(const INSTRUCTION_OPHAND d) {
+		if (V) {
+			PC += d.value;
+		}
+	}
+    void OP_CLC(const INSTRUCTION_OPHAND d) {
+		C = false;
+	}
+    void OP_CLD(const INSTRUCTION_OPHAND d) {
+		D = false;
+	}
+    void OP_CLI(const INSTRUCTION_OPHAND d) {
+		I = false;
+	}
+    void OP_CLV(const INSTRUCTION_OPHAND d) {
+		V = false;
+	}
+    void OP_CMP(const INSTRUCTION_OPHAND d) {
+		result = A - d.value;
+		Z = (result==0);
+		N = (result&SIGN_BIT)==SIGN_BIT;
+		C = d.value<=A;
+
+	}
+    void OP_CPX(const INSTRUCTION_OPHAND d) {
+		result = X - d.value;
+		Z = (d.value==X);
+		N = (result&SIGN_BIT)==SIGN_BIT;
+		C = d.value>=X;
+	}
+    void OP_CPY(const INSTRUCTION_OPHAND d) {
+		result = signedsub8(Y, d.value);
+		Z = (d.value==Y);
+		N = (result&SIGN_BIT)==SIGN_BIT;
+		C = Y>=d.value;
+	}
+	void OP_DEC(const INSTRUCTION_OPHAND d) {
+		RAM[d.address] += (~1 + 1);
+	}
+	void OP_DEX(const INSTRUCTION_OPHAND d) {
+		X -= 1;
+		N = (X&SIGN_BIT)==SIGN_BIT;
+		Z = X==0;
+	}
+	void OP_DEY(const INSTRUCTION_OPHAND d) {
+		Y -= 1;
+		N = (Y&SIGN_BIT)==SIGN_BIT;
+		Z = Y==0;
+	}
+	void OP_EOR(const INSTRUCTION_OPHAND d) {
+		A ^= d.value;
+		Z = A==0;
+		N = (A&SIGN_BIT)==SIGN_BIT;
+	}
+
 };
